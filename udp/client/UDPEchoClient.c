d4d593bc336dc3ebc3a0c19cc5185de5fb663991
kkdosk <kkdosk@naver.com>
2019-06-20 23:56:17 +0900

    add udp client

--- udp/client/UDPEchoClient.c
+++ udp/client/UDPEchoClient.c
@@ -21,9 +21,47 @@ int main(int argc, char *argv[]) {
   int echoStringLen;
   int respStringLen;
 
-  if ((argc < 3) || (argc >4)) {
+  if ((argc < 3) || (argc > 4)) {
     fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port]\n", argv[0]);
     exit(1);
   }
+
+  servIP = argv[1];
+  echoString = argv[2];
+
+  if ((echoStringLen = strlen(echoString)) > ECHOMAX)
+    DieWithError("Echo word too long");
+
+  if (argc == 4)
+    echoServPort = atoi(argv[3]);
+  else
+    echoServPort = 7;
+
+  if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+    DieWithError("socket() failed");
+
+  memset(&echoServAddr, 0, sizeof(echoServAddr));
+  echoServAddr.sin_family = AF_INET;
+  echoServAddr.sin_addr.s_addr = inet_addr(servIP);
+  echoServAddr.sin_port = htons(echoServPort);
+
+  if (sendto(sock, echoString, echoStringLen, 0, (struct sockaddr *)
+    &echoServAddr, sizeof(echoServAddr)) != echoStringLen)
+    DieWithError("sendto() sent a different numbe of bytes than expected");
+
+  fromSize = sizeof(fromAddr);
+  if ((respStringLen = recvfrom(sock, echoBuffer, ECHOMAX, 0,
+    (struct sockaddr *)&fromAddr, &fromSize)) != echoStringLen)
+    DieWithError("recvfrom() failed");
+  
+  if (echoServAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
+    fprintf(stderr, "Error: received a packet from unknown source.\n");
+    exit(1);
+  }
+
+  echoBuffer[respStringLen] = '\0';
+  printf("Received: %s\n", echoBuffer);
+
+  close(sock);
   return 0;
 }
--- udp/client/client.sh
+++ udp/client/client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /Users/kidaekwon/study/c/network/udp/client
+cd $PWD/udp/client
 gcc -o UDPEchoClient UDPEchoClient.c -L../../ -lDieWithError.o && ./UDPEchoClient 127.0.0.1 "hello" 5050
