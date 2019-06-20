e2a3b9e40719cf559d13dc31efe16a774fa555cd
kwon <kkdosk@naver.com>
2019-06-20 18:15:11 +0900

    udp

---
+++ udp/client/client.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ECHOMAX 255
+
+void DieWithError(char *errorMessage);
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
