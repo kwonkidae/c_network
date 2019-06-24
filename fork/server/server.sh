627276f3aa786e3ec0a7efe60182209eebdfb020
kwon <kkdosk@naver.com>
2019-06-24 13:31:08 +0900

    add

--- fork/server/server.sh
+++ fork/server/server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd $PWD/fork/server
-gcc -o TCPEchoServer-Fork TCPEchoServer-Fork.c TCPEchoServer.c AcceptTCPConnection.c -L../../ -lDieWithError.o -I./ \
+gcc -o TCPEchoServer-Fork TCPEchoServer-Fork.c CreateTCPServerSocket.c AcceptTCPConnection.c \
+  ../../tcp/server/HandleTCPClient.c -L../../ -lDieWithError.o -I./ \
   && ./TCPEchoServer-Fork 5050
